23b4b03d4e3fb72e058b14def520889db54392b1
Olena <shapovalev@gmail.com>
2023-09-15 10:01:28 +0200

    add-localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,80 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import { Container, Title, TitleList } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const localData = JSON.parse(localStorage.getItem('contacts'));
+    if (localData && localData.length > 0) {
+      this.setState({ contacts: localData });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  createContact = dataFromForm => {
+    const isContactExist = this.state.contacts.find(
+      contact => contact.name === dataFromForm.name
+    );
+    if (isContactExist) {
+      return alert(`${dataFromForm.name} is already in contacts.`);
+    }
+
+    const newContact = {
+      ...dataFromForm,
+      id: nanoid(),
+    };
+    this.setState(prev => ({
+      contacts: [...prev.contacts, newContact],
+    }));
+  };
+
+  handleFilter = ({ target: { value } }) => {
+    this.setState({ filter: value });
+  };
+
+  handleDelete = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  createContactsByfilter = () => {
+    if (!this.state.filter) {
+      return;
+    }
+    const ContactsByFilter = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+    return ContactsByFilter;
+  };
+
+  render() {
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm createContact={this.createContact} />
+        <TitleList>Contacts</TitleList>
+        <Filter handleFilter={this.handleFilter} />
+        <ContactList
+          contacts={this.createContactsByfilter() ?? this.state.contacts}
+          handleDelete={this.handleDelete}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
